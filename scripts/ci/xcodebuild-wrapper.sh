28e523dbfbef7cecdc6bd5a1bde0f3d0f9e98fd2
polybeta2 <polybeta114@gmail.com>
2026-09-11 19:30:19 +0800

    ci(ios): fix workflow YAML and use checked-in xcodebuild wrapper

---
+++ scripts/ci/xcodebuild-wrapper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+DEVELOPER_DIR=$(xcode-select -p)
+REAL="${DEVELOPER_DIR}/usr/bin/xcodebuild.real"
+
+NEW_ARGS=()
+for arg in "$@"; do
+  if [ "$arg" != "-allowProvisioningUpdates" ]; then
+    NEW_ARGS+=("$arg")
+  fi
+done
+
+NEW_ARGS+=(
+  CODE_SIGNING_ALLOWED=NO
+  CODE_SIGNING_REQUIRED=NO
+  CODE_SIGN_IDENTITY=""
+  CODE_SIGN_ENTITLEMENTS=""
+  DEVELOPMENT_TEAM=""
+)
+
+echo "[xcodebuild-wrapper] Executing real xcodebuild with code signing disabled"
+exec "$REAL" "${NEW_ARGS[@]}"
